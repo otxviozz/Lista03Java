cf726de6d20d0c5e53d92bebdbbea890c9e776c8
otavio <otaviotemoteo@gmail.com>
2025-04-04 15:19:52 -0300

    Exercício 10
    resolução exercício 10

---
+++ Lista03/src/Ex10.java
@@ -0,0 +1,21 @@
+package exercises;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		int[] arr1 = {5, 7, 9, 11, 13, 15, 17, 19, 21, 23};
+		int[] arr2 = new int[arr1.length];
+		
+		for(int i=0; i<arr1.length; i++) {
+		 arr2[i] =  arr1[i]%2;
+			}
+		System.out.print("Array 1: ");
+		for(int i=0; i<arr1.length; i++) {
+			System.out.print(arr1[i] + " ");
+		}
+		System.out.print("\nArray 2: ");
+		for(int i=0; i<arr2.length; i++) {
+			System.out.print(arr2[i] + " ");
+		}
+	}
+}
