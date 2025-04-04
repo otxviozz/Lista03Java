b2e405ce759e7d7504ad764b0e9aba4496ec1e85
otavio <otaviotemoteo@gmail.com>
2025-04-04 15:17:59 -0300

    Exercicio 2
    resolução exercício 2

---
+++ Lista03/src/Ex2.java
@@ -0,0 +1,22 @@
+// Create two arrays, the first one has values, the second one you have to assign twice the values of the first one.
+package exercises;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		int[] arr1 = {10, 20, 30, 40, 50, 60, 70, 80};
+		int[] arr2 = new int[arr1.length];
+		
+		for(int i=0; i<arr1.length; i++) {
+		 arr2[i] =  arr1[i]*2;
+			}
+		System.out.print("Array 1: ");
+		for(int i=0; i<arr1.length; i++) {
+			System.out.print(arr1[i] + " ");
+		}
+		System.out.print("\nArray 2: ");
+		for(int i=0; i<arr2.length; i++) {
+			System.out.print(arr2[i] + " ");
+		}
+	}
+}
