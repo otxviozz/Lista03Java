e1e7a2dd6d77a334bd50976d3a3cbd38ba588493
otavio <otaviotemoteo@gmail.com>
2025-04-04 15:19:05 -0300

    Exercício 6
    resolução exercício 6

---
+++ Lista03/src/Ex6.java
@@ -0,0 +1,27 @@
+// Create two arrays, the first and the second one has values, and the third has to be the result of array1+array2
+package exercises;
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		int[] arr1 = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		int[] arr2 = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		int[] arr3 = new int[arr1.length];
+		
+		for(int i=0; i<arr1.length; i++) {
+			 arr3[i] =  arr1[i]+arr2[i];
+				}
+			System.out.print("Array 1: ");
+			for(int i=0; i<arr1.length; i++) {
+				System.out.print(arr1[i] + " ");
+			}
+			System.out.print("\nArray 2: ");
+			for(int i=0; i<arr2.length; i++) {
+				System.out.print(arr2[i] + " ");
+			}
+			System.out.print("\nArray 3: ");
+			for(int i=0; i<arr3.length; i++) {
+				System.out.print(arr3[i] + " ");
+			}
+	}
+}
