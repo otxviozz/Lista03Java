217174f535cda2e1621f0cb9cc9f68c29f3a1f1a
otavio <otaviotemoteo@gmail.com>
2025-04-04 15:19:38 -0300

    Exercício 9
    resolução exercício 9

---
+++ Lista03/src/Ex9.java
@@ -0,0 +1,27 @@
+package exercises;
+
+public class Ex9 {
+
+	public static void main(String[] args) {
+		int [] arr1 = {12, 18, 20, 28, 30, 36, 40, 45, 48, 54};
+		int []arr2 = {3, 3, 4, 7, 5, 6, 8, 9, 6, 6};
+		int[] arr3 = new int[arr1.length];
+		
+		for(int i=0; i<arr1.length; i++) {
+			 arr3[i] =  arr1[i]/arr2[i];
+				}
+			System.out.print("Array 1: ");
+			for(int i=0; i<arr1.length; i++) {
+				System.out.print(arr1[i] + " ");
+			}
+			System.out.print("\nArray 2: ");
+			for(int i=0; i<arr2.length; i++) {
+				System.out.print(arr2[i] + " ");
+			}
+			System.out.print("\nArray 3: ");
+			for(int i=0; i<arr3.length; i++) {
+				System.out.print(arr3[i] + " ");
+			}
+	}
+}
+
