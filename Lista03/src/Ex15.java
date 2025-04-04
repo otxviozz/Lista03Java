e30d18946407e65f9e881bb8e6d5935b1a98558a
otavio <otaviotemoteo@gmail.com>
2025-04-04 15:20:49 -0300

    Exercício 15
    resolução exercício 15

---
+++ Lista03/src/Ex15.java
@@ -0,0 +1,26 @@
+package exercises;
+
+public class Ex15 {
+
+	public static void main(String[] args) {
+		int[] array = {2, 5, 8, 11, 14, 17, 20, 23, 26, 29};
+		int par=0;
+		int impar=0;
+		
+		System.out.print("Array: ");
+		for(int i=0; i<array.length; i++) {
+			System.out.print(array[i]+" ");
+		}
+		
+		for(int i=0; i<array.length; i++) {
+			if(array[i]%2==0) {
+			par= par+10;
+			}
+			else {
+			impar= impar +10; 
+			}
+		}
+		System.out.println("\nDessa array, "+par+"% é par e "+impar+"% é ímpar.");
+	}
+
+}
