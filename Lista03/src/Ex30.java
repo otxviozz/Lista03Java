ee41bcfe021e73dd0d6308b98c4750bc52ec2076
otavio <otaviotemoteo@gmail.com>
2025-04-21 22:52:15 -0300

    Ex30

---
+++ Lista03/src/Ex30.java
@@ -0,0 +1,46 @@
+package lista03;
+
+public class Ex30 {
+
+	public static void main(String[] args) {
+		int[][] matriz = new int[4][4];
+		int maior=0;
+		int linha=0;
+		int coluna=0;
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+		    	matriz[i][j]= (int)(Math.random() * 10);
+		    }
+		}
+	
+	for (int i = 0; i < matriz.length; i++) {            
+	    for (int j = 0; j < matriz[i].length; j++) {
+	    	System.out.print(matriz[i][j] + " ");
+	    }
+	    System.out.println(); 
+	}
+	
+	maior=matriz[0][0];
+	for (int i = 0; i < matriz.length; i++) {            
+	    for (int j = 0; j < matriz[i].length; j++) {
+	    	if(maior<matriz[i][j]) {
+	    		maior=matriz[i][j];
+	    		linha=i;
+	    		coluna=j;
+	    	}
+	    }
+	}
+	System.out.println("Maior número da matriz: "+maior);
+	System.out.println("Ele está na posição ("+linha+","+coluna+").");
+	for (int i = 0; i < matriz.length; i++) {            
+	    for (int j = 0; j < matriz[i].length; j++) {
+	    	if(maior==matriz[i][j] && i!=linha && j!=coluna) {
+	    		linha =i;
+	    		coluna=j;
+	    		System.out.println("Ele está na posição ("+linha+","+coluna+").");
+	    	}
+	    }
+	    }
+	}
+}
