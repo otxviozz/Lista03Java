6d8b7701fac4caae4372fa8d75fe6d67915b8735
otavio <otaviotemoteo@gmail.com>
2025-04-25 14:45:21 -0300

    Ex18Goal

--- Lista03/src/Ex18.java
+++ Lista03/src/Ex18.java
@@ -1,3 +1,5 @@
+//Read a vector A with 10 integer elements corresponding to the ages of a group of people.
+//Write a program that determines and writes the youngest and oldest ages.
 package lista03;
 
 public class Ex18 {
