97fdfe791f65c27be855dca114404877695a9fcf
otavio <otaviotemoteo@gmail.com>
2025-04-25 14:41:46 -0300

    Ex14Goal

--- Lista03/src/Ex14.java
+++ Lista03/src/Ex14.java
@@ -1,3 +1,6 @@
+//Create a vector A with 10 integer elements. Implement a program that
+//defines and writes the simple arithmetic mean of the odd elements
+//stored in this vector.
 package exercises;
 
 public class Ex14 {
