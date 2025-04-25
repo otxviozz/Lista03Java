87cb72cf5dc72452bb3d36f8ddc6c87ee61ef655
otavio <otaviotemoteo@gmail.com>
2025-04-25 14:48:02 -0300

    Ex22Goal

--- Lista03/src/Ex22.java
+++ Lista03/src/Ex22.java
@@ -1,3 +1,5 @@
+//Read a vector A with 10 elements and construct a vector B of the same type
+//and size and with the same elements as A, although these must be inverted.
 package lista03;
 
 public class Ex22 {
